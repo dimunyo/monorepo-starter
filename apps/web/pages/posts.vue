eca10323b103ee14b1b4c4ab25dd518f2970c148
Mekan Flores <mekan.flores@go.team>
2025-01-06 13:51:53 +0800

    feat: add routing and  icons

---
+++ apps/web/pages/posts.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+ 
+const { data } = useFetch<any>('http://localhost:8000/posts')
+ 
+const columns = [
+  {
+    header: 'id',
+    accessorKey: 'id',
+  },
+  {
+    header: 'title',
+    accessorKey: 'title',
+  },
+  {
+    header: 'Description',
+    accessorKey: 'description',
+  },
+]
+</script>
+ 
+<template>
+<NTable
+    :columns
+    :data="data?.posts"
+  />
+</template>
